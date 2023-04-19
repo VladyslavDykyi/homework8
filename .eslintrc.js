489964101e9a94e8592d38f22532720509ee25c7
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2023-04-19 19:18:52 +0300

    end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": "airbnb-base",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    extends: 'airbnb-base',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
     },
-    "rules": {
-        "no-plusplus": "off",
-        "strict": 0
-    }
-}
+    rules: {
+        'no-plusplus': 'off',
+        strict: 0,
+        quotes: 0,
+        indent: 0,
+    },
+};
--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@
 const getSum = (a, b) => a + b;
 getSum(1, 5);
 
-const getSubtraction = (a, b) => a + b;
+const getSubtraction = (a, b) => a - b;
 getSubtraction(1, 5);
 
-const getMultiplication = (a, b) => a + b;
+const getMultiplication = (a, b) => a * b;
 getMultiplication(1, 5);
