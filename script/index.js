9538aecb6ba387034b285e5d7a876dc5ca736064
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2023-04-19 19:17:38 +0300

    create home work 8

---
+++ script/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const getSum = (a, b) => a + b;
+getSum(1, 5);
+
+const getSubtraction = (a, b) => a + b;
+getSubtraction(1, 5);
+
+const getMultiplication = (a, b) => a + b;
+getMultiplication(1, 5);
